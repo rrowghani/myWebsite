65cbf5b9fa2d6bd619039f80b7049b4ae2f049c3
Reza Rowghani <reza.rowghani@gmail.com>
2023-09-20 02:49:45 +0200

    First version complete. Tested

--- src/main/java/com/rezarowghani/controllers/ContactMe.java
+++ src/main/java/com/rezarowghani/controllers/ContactMe.java
@@ -1,5 +1,6 @@
 package com.rezarowghani.controllers;
 
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
@@ -12,12 +13,14 @@ import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 import com.amazonaws.regions.Regions;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Objects;
+
 @Controller
 public class ContactMe {
 
     // Replace recipient@example.com with a "To" address. If your account
     // is still in the sandbox, this address must be verified.
-    static final String TO = "info@rezarowghani.com";
+    static final String TO = "reza.rowghani@gmail.com";
     static final String FROM = "info@rezarowghani.com";
     // The subject line for the email.
     static final String SUBJECT = "Message from a user from your website";
@@ -30,11 +33,12 @@ public class ContactMe {
     )  {
 
         try {
-            AmazonSimpleEmailService client =
-                    AmazonSimpleEmailServiceClientBuilder.standard()
-                            // Replace US_WEST_2 with the AWS Region you're using for
-                            // Amazon SES.
-                            .withRegion(Regions.EU_NORTH_1).build();
+
+            AmazonSimpleEmailServiceClientBuilder amazonSimpleEmailServiceClientBuilder = AmazonSimpleEmailServiceClientBuilder.standard()
+                    // Replace US_WEST_2 with the AWS Region you're using for
+                    // Amazon SES.
+                    .withRegion(Regions.EU_NORTH_1);
+            AmazonSimpleEmailService client = Objects.equals(System.getenv("ENVIRONMENT"), "PRODUCTION") ? amazonSimpleEmailServiceClientBuilder.withCredentials(InstanceProfileCredentialsProvider.getInstance()).build() : amazonSimpleEmailServiceClientBuilder.build();
 
             String emailBody = "Name: " + name + "\n" +
                     "Email: "  + email + "\n" +
@@ -49,7 +53,7 @@ public class ContactMe {
                                             .withCharset("UTF-8").withData(emailBody)))
                             .withSubject(new Content()
                                     .withCharset("UTF-8").withData(SUBJECT)))
-                    .withSource(FROM);
+                    .withSource (FROM);
             client.sendEmail(request);
             System.out.println("Email sent!");
         } catch (Exception ex) {
