fd702ef521d024645696a679dc6b6ce1c5a5bc1a
Reza Rowghani <reza.rowghani@gmail.com>
2023-09-21 18:08:22 +0200

    Navbar updated and email result will be printed.

--- src/main/java/com/rezarowghani/controllers/ContactMe.java
+++ src/main/java/com/rezarowghani/controllers/ContactMe.java
@@ -1,17 +1,15 @@
 package com.rezarowghani.controllers;
 
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
-import com.amazonaws.services.simpleemail.model.Body;
-import com.amazonaws.services.simpleemail.model.Content;
-import com.amazonaws.services.simpleemail.model.Destination;
-import com.amazonaws.services.simpleemail.model.Message;
-import com.amazonaws.services.simpleemail.model.SendEmailRequest;
-import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.model.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Objects;
 
@@ -26,11 +24,11 @@ public class ContactMe {
     static final String SUBJECT = "Message from a user from your website";
 
     @RequestMapping(value = "/sendEmail", params = {"name", "email", "message"})
-    public String sendEmail(
+    public ModelAndView sendEmail(
             @RequestParam(value = "name") String name,
-            @RequestParam(value="email") String email,
+            @RequestParam(value = "email") String email,
             @RequestParam(value = "message") String message
-    )  {
+    ) {
 
         try {
 
@@ -41,7 +39,7 @@ public class ContactMe {
             AmazonSimpleEmailService client = Objects.equals(System.getenv("ENVIRONMENT"), "PRODUCTION") ? amazonSimpleEmailServiceClientBuilder.withCredentials(InstanceProfileCredentialsProvider.getInstance()).build() : amazonSimpleEmailServiceClientBuilder.build();
 
             String emailBody = "Name: " + name + "\n" +
-                    "Email: "  + email + "\n" +
+                    "Email: " + email + "\n" +
                     "Message: " + message;
 
             SendEmailRequest request = new SendEmailRequest()
@@ -53,15 +51,23 @@ public class ContactMe {
                                             .withCharset("UTF-8").withData(emailBody)))
                             .withSubject(new Content()
                                     .withCharset("UTF-8").withData(SUBJECT)))
-                    .withSource (FROM);
+                    .withSource(FROM);
             client.sendEmail(request);
             System.out.println("Email sent!");
         } catch (Exception ex) {
             System.out.println("The email was not sent. Error message: "
                     + ex.getMessage());
+            return new ModelAndView("redirect:/actionResult?result=failure");
         }
-        return "contactMe";
+        return new ModelAndView("redirect:/actionResult?result=success");
     }
 
-
+    @RequestMapping(value = "/actionResult", params = {"result"})
+    public String printResult(
+            @RequestParam(value = "result") String result,
+            Model model
+    ) {
+        model.addAttribute("result", result);
+        return "actionResult";
+    }
 }
